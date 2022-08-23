259427ee751f127ec938fdd8529b975c6a872548
Soumyadeep Nag <soumyanag16@gmail.com>
2022-08-23 19:01:11 +0530

    uploaded files

---
+++ frac_knapsack_rec.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float *pw, *p, *w, *x;
+
+int max(int n){
+	int i, pos;
+	float mx = 0;
+	for(i=0; i<n; i++){
+		if(pw[i] > mx){
+			mx = pw[i];
+			pos = i;	
+		}
+	}
+	pw[pos] = 0;
+	return pos;
+} 
+
+void knapsack(int n, int M, int wt){
+	int i = max(n);	
+		
+	if((wt + w[i]) <= M){
+		wt += w[i];
+		x[i] = 1;
+		knapsack(n, M, wt);
+	}
+	else{
+		// fractional part
+		x[i] = (M-wt)/w[i];
+		wt = M; 
+	}
+	return;	
+}
+
+int main(){
+	int n, i, max;
+	
+	printf("Enter no. of objects: ");
+	scanf("%d", &n);
+	p = (float*)calloc(0, n*sizeof(float));
+	w = (float*)calloc(0, n*sizeof(float));
+	pw = (float*)calloc(0, n*sizeof(float));
+	x = (float*)calloc(0, n*sizeof(float));
+	
+	printf("Enter the profit values: ");
+	for(i=0; i<n; i++)
+		scanf("%f", &p[i]);
+		
+	printf("Enter the weights: ");
+	for(i=0; i<n; i++)
+		scanf("%f", &w[i]);
+		
+	printf("Enter the max weight limit: ");
+	scanf("%d", &max);
+	
+	for(i=0; i<n; i++){
+		x[i] = 0;		
+		pw[i] = p[i]/w[i];
+	}
+	
+	printf("Profit-weight ratio (p/w) values:\n");
+	for(i=0; i<n; i++)
+		printf("%.2f\t", pw[i]);
+	
+	knapsack(n, max, 0);
+	
+	printf("\nObject vector:");
+	for(i=0; i<n; i++)
+		printf("\t%.2f", x[i]);
+	
+	return 0;
+}
